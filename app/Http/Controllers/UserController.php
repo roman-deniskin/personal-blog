8855d9038391463e60587b47cecc380dab88c028
Voland <roman-deniskin@yandex.ru>
2019-07-10 02:21:55 +0200

    Add profile edit, admin panel views and routes navigation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserUpdate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use \Illuminate\View\View;
 
 class UserController extends Controller
 {
@@ -12,11 +15,32 @@ class UserController extends Controller
     }*/
     // TODO: Добавить проверку, что пользователь авторизован (через закомментированный мидлваре)
 
+    /**
+     * @return View
+     */
     public function dashboard() {
         return View('user.dashboard');
     }
 
+    /**
+     * @return View
+     */
     public function comments() {
         return View('user.comments');
+    }
+
+    /**
+     * @return View
+     */
+    public function profile() {
+        return View('user.profile');
+    }
+
+    public function profilePost(UserUpdate $request) {
+        $user = Auth::user();
+        $user->name = $request['name'];
+        $user->email = $request['email'];
+        $user->save();
+        return back();
     }
 }
